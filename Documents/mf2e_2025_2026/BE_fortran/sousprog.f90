deb4b751f2d43c794eb0e3066a393925e47f93b4
Arthur FLAUDER <arthurfldr57@gmail.com>
2025-11-21 15:30:41 +0100

    ajout fonction H

--- Documents/mf2e_2025_2026/BE_fortran/sousprog.f90
+++ Documents/mf2e_2025_2026/BE_fortran/sousprog.f90
@@ -4,18 +4,33 @@ use m_type
 
 implicit none
 
-type(phys) :: p
+    type(phys) :: p
 
-open(10, nom_fichier)
-read(10,*) :: p%L
-read(10,*) :: p%tf
-read(10,*) :: p%C0
-read(10,*) :: p%U0
-read(10,*) :: p%N
-read(10,*) :: p%Nt
+    open(10, nom_fichier)
+    read(10,*) :: p%L
+    read(10,*) :: p%tf
+    read(10,*) :: p%C0
+    read(10,*) :: p%U0
+    read(10,*) :: p%N
+    read(10,*) :: p%Nt
 
-close(10)
+    close(10)
 
 end subroutine lecture_data
+
+function H(x)
+
+    implicit none
+
+    integer, intent(out) :: val
+    real, intent(in) :: x 
+
+    if (x<0) then
+        val = 1
+    else 
+        val = 0
+
+    end function 
+
 
 
